e80223e3954480b42a35ae166bb9ed5c48737b41
Rekha Puttaswamy <rputtaswamy@dotscreen.com>
2022-02-23 16:53:48 +0100

    Adding Navigation

--- app/src/main/java/com/example/composecontactslist/MainActivity.kt
+++ app/src/main/java/com/example/composecontactslist/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.composecontactslist
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,13 +14,22 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.example.composecontactslist.ui.MyTheme
@@ -29,20 +40,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                ContactsScreen()
+                ContactsApplication()
             }
         }
     }
 
     @Composable
-    fun ContactsScreen(contacts: List<Contacts> = contactsList) {
-        Scaffold(topBar = { AppBar() }) {
+    fun ContactsApplication(contacts: List<Contacts> = contactsList) {
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "contacts_list") {
+            composable("contacts_list") {
+                ContactsListScreen(contacts, navController)
+            }
+            composable(route = "contact_details/{id}",
+                       arguments = listOf(navArgument("id") {
+                           type = NavType.IntType
+                       })
+            ) {
+                navBackStackEntry ->
+                    ContactDetailsScreen(navBackStackEntry.arguments!!.getInt("id"), navController)
+            }
+        }
+    }
+
+    @Composable
+    fun ContactsListScreen(contacts: List<Contacts> = contactsList, navController: NavHostController?) {
+        Scaffold(topBar = {
+            AppBar(
+                title = "Users list",
+                icon = Icons.Default.AccountCircle
+            ) { }
+        }) {
             Surface(
                 modifier = Modifier.fillMaxSize(),
             ) {
                 LazyColumn {
                     items(contacts) { contact ->
-                        ContactCard(contact = contact)
+                        ContactCard(contact = contact) {
+                            navController?.navigate("contact_details/${contact.id}")
+                        }
                     }
                 }
             }
@@ -50,56 +86,85 @@ class MainActivity : AppCompatActivity() {
     }
 
     @Composable
-    fun AppBar() {
-        TopAppBar(
-            navigationIcon = {
-                Icon(
-                    Icons.Default.AccountCircle,
-                    contentDescription = "",
-                    modifier = Modifier.padding(horizontal = 12.dp),
-                )
-            },
-            title = { Text("My Contacts") }
-        )
-    }
-
-    @Composable
-    fun ContactCard(contact: Contacts) {
-        Card(
-            modifier = Modifier
-                .padding(top = 8.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
-                .fillMaxWidth()
-                .wrapContentHeight(align = Alignment.Top),
-            elevation = 8.dp,
-            backgroundColor = Color.White
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Start
+    fun ContactDetailsScreen(id: Int, navController: NavHostController?) {
+        val contactDetail = contactsList.first {contact -> id == contact.id }
+        Scaffold(topBar = {
+            AppBar(
+                title = "Contact Details",
+                icon = Icons.Default.ArrowBack
             ) {
-                ContactPicture(contact.imageUrl, contact.status)
-                ContactContent(contact.name, contact.status)
+                navController?.navigateUp()
             }
-
+        }) {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Column(modifier = Modifier.fillMaxWidth(),
+                       horizontalAlignment = Alignment.CenterHorizontally,
+                       verticalArrangement = Arrangement.Top) {
+                    ContactPicture(contactDetail.imageUrl, contactDetail.status, 220.dp)
+                    ContactContent(contactName = contactDetail.name, onlineStatus = contactDetail.status, Alignment.CenterHorizontally)
+                }
+            }
         }
     }
 
     @Composable
-    fun ContactPicture(imageUrl: String, onlineStatus: Boolean) {
+    fun AppBar(title: String, icon: ImageVector, iconClickAction: () -> Unit) {
+        TopAppBar(
+            navigationIcon = {
+                Icon(
+                    icon,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(horizontal = 12.dp)
+                        .clickable(onClick = { iconClickAction.invoke() })
+                )
+            },
+            title = { Text(title) }
+        )
+    }
+
+    @Composable
+    fun ContactCard(contact: Contacts, clickAction: () -> Unit) {
+        Card(
+            modifier = Modifier
+                .padding(top = 8.dp, bottom = 4.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+                .wrapContentHeight(align = Alignment.Top)
+                .clickable(onClick = { clickAction.invoke() }),
+            elevation = 8.dp,
+            backgroundColor = Color.White
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                ContactPicture(imageUrl = contact.imageUrl, status = contact.status, imageSize = 72.dp)
+                ContactContent(
+                    contactName = contact.name,
+                    onlineStatus = contact.status,
+                    alignment = Alignment.Start)
+            }
+        }
+    }
+
+    @Composable
+    fun ContactPicture(imageUrl: String, status: Boolean, imageSize: Dp) {
         Card(
             shape = CircleShape,
             border = BorderStroke(
                 width = 2.dp,
-                color = if (onlineStatus)
-                    MaterialTheme.colors.lightGreen
+                color = if(status)
+                     MaterialTheme.colors.lightGreen
                 else Color.Red
             ),
             modifier = Modifier
                 .padding(16.dp)
-                .size(72.dp),
+                .size(imageSize),
             elevation = 4.dp
-        ) {
+        ){
             Image(
                 painter = rememberImagePainter(
                     data = imageUrl,
@@ -108,17 +173,17 @@ class MainActivity : AppCompatActivity() {
                     },
                 ),
                 modifier = Modifier.size(72.dp),
-                contentDescription = "Contacts picture",
+                contentDescription = "Contact Icon description"
             )
         }
     }
 
     @Composable
-    fun ContactContent(contactName: String, onlineStatus: Boolean) {
+    fun ContactContent(contactName: String, onlineStatus: Boolean, alignment: Alignment.Horizontal) {
         Column(
             modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth()
+                .padding(8.dp),
+                horizontalAlignment = alignment
         ) {
             CompositionLocalProvider(
                 LocalContentAlpha provides (
@@ -144,9 +209,17 @@ class MainActivity : AppCompatActivity() {
 
     @Preview(showBackground = true)
     @Composable
-    fun DefaultPreview() {
+    fun ContactDetailsPreview() {
         MyTheme {
-            ContactsScreen()
+            ContactDetailsScreen(id = 0, null)
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun ContactsListPreview() {
+        MyTheme {
+            ContactsListScreen(contacts = contactsList, null)
         }
     }
 
